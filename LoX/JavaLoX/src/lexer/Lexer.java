a4f52f04b6e3cdfe85efdbed1ab2ab3e2183758f
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2023-11-30 13:18:51 +0600

    Implemented JavaLoX main REPL operations and error handling

---
+++ LoX/JavaLoX/src/lexer/Lexer.java
@@ -0,0 +1,19 @@
+package lexer;
+
+import token.Token;
+
+import java.util.List;
+
+public class Lexer {
+    private final String source;
+
+    public Lexer(String source) {
+        this.source = source;
+    }
+
+    public List<Token> scanTokens() {
+        return null;
+    }
+
+    private void scanToken() {}
+}
