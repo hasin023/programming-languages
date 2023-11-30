133e1b2e9a7b23089b02654f8c3aa7d8b5afa108
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2023-11-30 14:24:17 +0600

    Added Token class for JavaLoX

--- LoX/JavaLoX/src/token/Token.java
+++ LoX/JavaLoX/src/token/Token.java
@@ -1,4 +1,35 @@
 package token;
 
 public class Token {
+    public final TokenType tokenType;
+    public String lexeme;
+    public Object literal;
+    public final int line;
+
+    public Token(TokenType type, String lexeme, int line) {
+        this.tokenType = type;
+        this.lexeme = lexeme;
+        this.literal = null;
+        this.line = line;
+    }
+
+    public Token(TokenType tokenType, String lexeme, Object literal, int line) {
+        this.tokenType = tokenType;
+        this.lexeme = lexeme;
+        this.literal = literal;
+        this.line = line;
+    }
+
+    @Override
+    public String toString() {
+        return tokenType + " " + lexeme + " " + literal;
+    }
+
+    public void concat(Token token) {
+        if (token == null) {
+            return;
+        }
+
+        this.lexeme += token.lexeme;
+    }
 }
